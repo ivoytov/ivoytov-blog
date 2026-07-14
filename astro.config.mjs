6953631aba163c5b357438fa42d3e841e11aa1dd
Ilya Voytov <ilya@voytov.com>
2026-07-13 21:02:21 -0400

    fix: restore cloudflare deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://voytov.com",
   integrations: [mdx(), sitemap(), tailwind()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
